1bfadc6d0cc961af61c995184f7d98eddd22464a
Joe San <codeintheopen@gmail.com>
2017-04-18 22:19:46 +0200

    Update Functor.scala

--- Functor.scala
+++ Functor.scala
@@ -7,14 +7,17 @@ trait Functor[F[_]] {
   def map[A, B](fa: F[A])(f: A => B): F[B]
 }
 
-// Let's now implement a OptionFunctor (as a type class)
-implicit val OptionFunctor = new Functor[Option] {
-  def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa match {
-    case Some(a) => Some(f(a))
-    case None    => None
-}
+object Functor {
+  // Let's now implement a OptionFunctor (as a type class)
+  implicit val OptionFunctor = new Functor[Option] {
+    def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa match {
+      case Some(a) => Some(f(a))
+      case None    => None
+    }
+  }
 
-// A ListFunctor
-implicit val ListFunctor = new Functor[List] {
-  def map[A, B](fa: List[A])(f: A => B): List[B] = fa map f 
+  // A ListFunctor
+  implicit val ListFunctor = new Functor[List] {
+    def map[A, B](fa: List[A])(f: A => B): List[B] = fa map f  
+  }
 }
