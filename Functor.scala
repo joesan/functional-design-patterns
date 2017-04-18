b52eba6569cadea28df22fd4abe50e4eba9fb0bb
Joe San <codeintheopen@gmail.com>
2017-04-18 22:47:42 +0200

    Update Functor.scala

--- Functor.scala
+++ Functor.scala
@@ -2,6 +2,9 @@
  * A functor is a higher kinded type that takes in
  * a type constructor as its argument and provides
  * a map function!
+ * In simple terms, a Functor is just a mapping 
+ * between categories, i.e., given two categories
+ * A and B a Functor F maps the category A to category B 
  */
 trait Functor[F[_]] {
   def map[A, B](fa: F[A])(f: A => B): F[B]
@@ -20,4 +23,12 @@ object Functor {
   implicit val ListFunctor = new Functor[List] {
     def map[A, B](fa: List[A])(f: A => B): List[B] = fa map f  
   }
+  
+  // Or any random Functor
+  case class User[A](param1: A, param2: A)
+  implicit val UserFunctor = new Functor[User] {
+    def map[A, B](fa: User[A])(f: A => B): User[B] = User(f(fa.param1), f(fa.param2)) 
+  }
+  
+  // Hmmm... a Function Functor... why not!
 }
