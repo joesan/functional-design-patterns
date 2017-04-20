436c6ea6c9c490bdee1132cf7af482b3a754c7b7
Joe San <codeintheopen@gmail.com>
2017-04-20 12:19:25 +0200

    Update Monoid.scala

--- Monoid.scala
+++ Monoid.scala
@@ -1,5 +1,5 @@
 /**
- * A Monoid is a type that defines the following laws:
+ * A Monoid is some type A that defines the following laws:
  * 1. A combinator operator which satisfies assosiativity
  * 2. A identity operator
  */
@@ -24,3 +24,21 @@
    def combine(a1: List[A], a2: List[A]) = a1 ++ a2
    def identity = List[A].empty
  }
+
+ val optionMonoid1 = new Monoid[Option[A]] {
+   def combine(a1: Option[A], a2: Option[A2]) a1 orElse a2
+   def identity = None
+ }
+
+ // a much better version of the OptionMonoid, using a Semigroup from cats
+ import cats._
+ def optionMonoid2[A: Semigroup]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    def combine(a1: Option[A], a2: => Option[A]) = (a1, a2) match {
+      case (Some(a), Some(b)) => Some(Semigroup[A].append(a, b))
+      case (Some(a), None)    => a
+      case (None, Some(b))    => b
+      case (None, None)       => None
+    }
+
+    def identity: Option[A] = None
+  }
